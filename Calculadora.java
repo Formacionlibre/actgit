86aa5ec636adf7ab74b0730397895ab195c00ba6
Belen Fdez Diaz <formacionlibre10@gmail.com>
2018-06-26 17:51:49 +0200

    Eliminacion de varios métodos
    Se ha eliminado varios metodos de la clase para hacer una modificación
    del código

--- Calculadora.java
+++ Calculadora.java
@@ -1,9 +1,38 @@
 
 public class Calculadora {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	private int num1;
+	private int num2;
+
+public Calculadora(int a, int b) {
+	num1 = a;
+	num2 = b;
+}
+public int suma() {
+	int resul = num1 + num2;
+	return resul;
+}
+public int resta() {
+	int resul = num1 - num2;
+	return resul;
+}
+	
+
+	
+
+
+        
+	
+	
+
+
+	
+
+	
+
+
+
 
 	}
 
-}
+
